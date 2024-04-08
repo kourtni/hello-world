7fba0b53b743abca948bd083d63b8e5b456d9e9b
Kourtni Marshall <kourtni.marshall@gmail.com>
2024-04-08 02:23:36 -0500

    Updates flake per rules_nixpkgs guide

--- flake.nix
+++ flake.nix
@@ -1,34 +1,53 @@
-{
+{ # This flake is heavily cribbed from
+  # https://github.com/tweag/rules_nixpkgs/blob/master/guide.md#nix-development-shells
+
   description = "Hello World Monorepo";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # Track a specific tag on the nixpkgs repo.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    # The flake format itself is very minimal, so the use of this
+    # library is common. For more details see:
+    # https://github.com/numtide/flake-utils
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      repoName = "The World of Helloz";
-  in 
-  {
+  # Here we can define various kinds of "outputs": packages, tests, 
+  # and so on, but we will only define a development shell.
+  outputs = { nixpkgs, flake-utils, ... }:
 
-    devShells.${system}.default =
-      pkgs.mkShell
-        {
-          buildInputs = [
-            pkgs.bazel
-            pkgs.vim
-          ];
+    # For every platform that Nix supports, we ...
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+          # ... get the package set for this particular platform ...
+          pkgs = nixpkgs.legacyPackages.${system};
+          repoName = "The World of Helloz";
+      in 
+      {
 
-          shellHook = ''
-            echo -e "\033[0;31m### Welcome to the ${repoName}. Remember to commit early & often! ###\033[0m"
-            echo -e "\033[0;32mThis shell provides the following development tools:\033[0m"
-            nix --version
-            bazel --version
-            echo "vim"
-          '';
-        };
+        # ... and define a development shell for it ...
+        devShells.default =
 
-  };
+          # ... with no globally-available CC toolchain ...
+          pkgs.mkShellNoCC {
+            name = "rules_nixpkgs_shell";
+
+            # ... which makes available the following dependencies, 
+            # all sourced from the `pkgs` package set:
+            packages = with pkgs; [
+              bazel
+              bazel-buildtools
+              vim
+            ];
+
+            shellHook = ''
+              echo -e "\033[0;31m### Welcome to the ${repoName}. Remember to commit early & often! ###\033[0m"
+              echo -e "\033[0;32mThis shell provides the following development tools:\033[0m"
+              nix --version
+              bazel --version
+              echo "vim"
+            '';
+          };
+      });
 }
